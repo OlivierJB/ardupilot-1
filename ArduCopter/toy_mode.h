SkyRocketToys/ardupilot

Copter: Added LED disarm pattern request - AF-25

ArduCopter/toy_mode.h
@@ -122,6 +122,7 @@ private:
     uint8_t green_blink_index;
     uint16_t red_blink_count;
     uint16_t green_blink_count;
+    uint16_t blink_disarm;
 
     // remember the last mode we set
     control_mode_t last_set_mode = LOITER;

OlivierJB/ardupilot-1

ArduCopter/toy_mode.h
@@ -127,6 +127,7 @@ private:
     uint8_t green_blink_index;
     uint16_t red_blink_count;
     uint16_t green_blink_count;
+    uint8_t blink_disarm;
 
     struct {
         AP_Float volt_min;